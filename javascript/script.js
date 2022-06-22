a8714ad58c894f3ab92e6a1ca78acf0921db9118
gapg3692 <gpaneca3692@gmail.com>
2022-06-22 15:04:58 -0400

    Complete exercise

--- javascript/script.js
+++ javascript/script.js
@@ -5,21 +5,44 @@ const Container = document.createElement("div");
 Container.classList.add("container");
 let color = "red";
 let boardColor = "black";
+let gridNumber = 16;
+let boardSize = 640;
 
-for (i = 1; i <= 256; i++) {
-    let div = document.createElement("div");
-    div.classList.add("grid");
-    gridContainer.appendChild(div);
+function createBoard(gridNumber, gridContainer) {
+    const divGridTemp = document.querySelectorAll(".grid");
+    divGridTemp.forEach((element) => gridContainer.removeChild(element));
+
+    for (i = 1; i <= gridNumber ** 2; i++) {
+        let div = document.createElement("div");
+        div.classList.add("grid");
+        gridContainer.appendChild(div);
+    }
+
+    gridContainer.style.gridTemplate = `repeat(${gridNumber}, auto) / repeat(${gridNumber}, auto)`;
 }
+
+function createGrid(gridNumber, boardSize, divGrid) {
+    divGrid.forEach(
+        (element) => (element.style.width = `${boardSize / gridNumber}px`)
+    );
+    divGrid.forEach(
+        (element) => (element.style.height = `${boardSize / gridNumber}px`)
+    );
+}
+
+createBoard(gridNumber, gridContainer);
 
 Container.appendChild(gridContainer);
 body.appendChild(Container);
 
-const divGrid = Array.from(document.querySelectorAll(".grid"));
+let divGrid = Array.from(document.querySelectorAll(".grid"));
 
-divGrid.forEach((element) =>
-    element.addEventListener("mouseenter", colorizeDiv)
-);
+function addEvent() {
+    divGrid = Array.from(document.querySelectorAll(".grid"));
+    divGrid.forEach((element) =>
+        element.addEventListener("mouseenter", colorizeDiv)
+    );
+}
 
 function colorizeDiv(event) {
     event.target.style.cssText = `background-color: ${color};`;
@@ -30,3 +53,18 @@ function clean() {
         (element) => (element.style.cssText = `background-color: ${boardColor};`)
     );
 }
+
+createGrid(gridNumber, boardSize, divGrid);
+addEvent();
+
+function buttonResize() {
+    gridNumber = Number(prompt("Insert grid size"));
+    if (gridNumber > 100) gridNumber = 100;
+    else if (gridNumber < 1) gridNumber = 1;
+    createBoard(gridNumber, gridContainer);
+    addEvent();
+    createGrid(gridNumber, boardSize, divGrid);
+}
+
+const button = document.getElementById("gridSize");
+button.addEventListener("click", () => buttonResize());
