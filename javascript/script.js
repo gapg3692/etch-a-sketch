f71761aea8582cc08aa1b6051f406190d53d03aa
gapg3692 <gpaneca3692@gmail.com>
2022-06-21 20:24:28 -0400

    Add grid and flex CSS style
    This commit add the grid css Style for the 16x16 divGrid, and a flex style for the container div.

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,17 @@
 const body = document.querySelector("body");
-const container = document.createElement("div");
+const gridContainer = document.createElement("div");
+gridContainer.classList.add("gridContainer");
+const Container = document.createElement("div");
+Container.classList.add("container");
+
 for (i = 1; i <= 256; i++) {
     let div = document.createElement("div");
     div.classList.add("grid");
-    container.appendChild(div);
+    gridContainer.appendChild(div);
 }
 
-body.appendChild(container);
+Container.appendChild(gridContainer);
+body.appendChild(Container);
 
 const divGrid = document.querySelectorAll(".grid");
 
